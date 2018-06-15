4718a53ccf79ca32c2c9f51973dfcfd72eb21cc9
yvesen <l.sen.t@hotmail.com>
2018-06-15 15:28:07 +0800

    kamigo day 1 to day 16

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/kamigo/eat', to: 'kamigo#eat'
+  get '/kamigo/request_headers', to: 'kamigo#request_headers'
+  get '/kamigo/request_body', to: 'kamigo#request_body'
+  get '/kamigo/response_headers', to: 'kamigo#response_headers'
+  get '/kamigo/response_body', to: 'kamigo#show_response_body'
+  get '/kamigo/sent_request', to: 'kamigo#sent_request'
+
+end
