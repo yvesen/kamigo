292592d1c9ff7802c90b84be83c8255673a174b3
yvesen <l.sen.t@hotmail.com>
2018-06-19 15:36:55 +0800

    修改回傳

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -2,6 +2,59 @@ require 'line/bot'
 class KamigoController < ApplicationController
 	protect_from_forgery with: :null_session
 
+	def webhook
+    # 設定回覆文字
+    reply_text = keyword_reply(received_text)
+
+    # 傳送訊息到line
+    response = reply_to_line(reply_text)
+      
+    # 回應 200
+    head :ok
+  end
+
+  # 取得對方說的話
+  def received_text
+    message = params['events'][0]['message']
+    message['text'] unless message.nil?
+  end
+
+  # 關鍵字回覆
+  def keyword_reply(received_text)
+    # 學習紀錄表
+    keyword_mapping = {
+      'QQ' => '神曲支援：https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s',
+      '我難過' => '神曲支援：https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s'
+    }
+    
+    # 查表
+    keyword_mapping[received_text]
+  end
+
+  # 傳送訊息到 line
+  def reply_to_line(reply_text)
+  	return nil if reply_text.nil?
+  	# 取得 reply token
+    reply_token = params['events'][0]['replyToken']
+
+    #設定回覆訊息
+    message = {
+    	type: 'text'
+    	text: reply_text
+    }
+    
+    # 傳送訊息
+    line.reply_message(reply_token, message)
+  end
+
+	# Line Bot API 物件初始化
+  def line
+    @line ||= Line::Bot::Client.new { |config|
+      config.channel_secret = 'cc045385cef60f08f97c691195f41270'
+      config.channel_token = 'qpypOGdgP+OaFoUf32PMi9swYJqHXeIh3To/wJwm6C8Su0fjMIR4LGTex7SKWJC9JkSCf7nwC4+kmuoRJShnXO17Guz21dk/oEoN72njzrPQa/pprsa8yfw9Z+DEifJ90c6UZw1hSlGNeH5Tcr/TcAdB04t89/1O/w1cDnyilFU='
+    }
+  end
+
 	def eat
 		render plain: "吃土吧"
 	end
@@ -48,50 +101,6 @@ class KamigoController < ApplicationController
 
   def translate_to_korean(message)
     "#{message}油~"
-  end
-
-  def webhook
-    # 設定回覆文字
-    reply_text = keyword_reply(received_text)
-    
-    # 傳送訊息到line
-    response = reply_to_line(reply_text)
-      
-    # 回應 200
-    head :ok
-  end
-
-  # 取得對方說的話
-  def received_text
-    params['events'][0]['message']['text']
-  end
-
-  # 關鍵字回覆
-  def keyword_reply(received_text)
-    received_text
-  end
-
-  # 傳送訊息到 line
-  def reply_to_line
-  	# 取得 reply token
-    reply_token = params['events'][0]['replyToken']
-
-    #設定回覆訊息
-    message = {
-    	type: 'text'
-    	text: reply_text
-    }
-    
-    # 傳送訊息
-    line.reply_message(reply_token, message)
-  end
-
-	# Line Bot API 物件初始化
-  def line
-    @line ||= Line::Bot::Client.new { |config|
-      config.channel_secret = 'cc045385cef60f08f97c691195f41270'
-      config.channel_token = 'qpypOGdgP+OaFoUf32PMi9swYJqHXeIh3To/wJwm6C8Su0fjMIR4LGTex7SKWJC9JkSCf7nwC4+kmuoRJShnXO17Guz21dk/oEoN72njzrPQa/pprsa8yfw9Z+DEifJ90c6UZw1hSlGNeH5Tcr/TcAdB04t89/1O/w1cDnyilFU='
-    }
   end
 
 end
