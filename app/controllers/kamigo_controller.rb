ae13e015770e2f465a4a67ffdad08603e3ab73e2
yvesen <l.sen.t@hotmail.com>
2018-06-20 16:49:26 +0800

    try fixed

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -23,6 +23,36 @@ class KamigoController < ApplicationController
     head :ok
   end 
 
+  # 頻道 ID
+  def channel_id
+    source = params['events'][0]['source']
+    source['groupId'] || source['roomId'] || source['userId']
+  end
+
+  # 儲存對話
+  def save_to_received(channel_id, received_text)
+    return if received_text.nil?
+    Received.create(channel_id: channel_id, text: received_text)
+  end
+
+  # 儲存回應
+  def save_to_reply(channel_id, reply_text)
+    return if reply_text.nil?
+    Reply.create(channel_id: channel_id, text: reply_text)
+  end
+  
+  def echo2(channel_id, received_text)
+    # 如果在 channel_id 最近沒人講過 received_text，卡米狗就不回應
+    recent_received_texts = Received.where(channel_id: channel_id).last(5)&.pluck(:text)
+    return nil unless received_text.in? recent_received_texts
+    
+    # 如果在 channel_id 卡米狗上一句回應是 received_text，卡米狗就不回應
+    last_reply_text = Reply.where(channel_id: channel_id).last&.text
+    return nil if last_reply_text == received_text
+
+    received_text
+  end
+
   # 取得對方說的話
   def received_text
     message = params['events'][0]['message']
@@ -45,36 +75,6 @@ class KamigoController < ApplicationController
 
     KeywordMapping.create(keyword: keyword, message: message)
     '好哦～好哦～'
-  end
-
-  #頻道ID
-  def channel_id
-  	source = params['events'][0]['source']
-  	source['groupId'] || source['roomId'] || source['userId']
-  end
-
-  #儲存對話
-  def save_to_received(channel_id, received_text)
-  	return if received_text.nil?
-  	Rceived.create(channel_id: channel_id, text:received_text)
-  end
-
-  # 儲存回應
-  def save_to_reply(channel_id, reply_text)
-    return if reply_text.nil?
-    Reply.create(channel_id: channel_id, text: reply_text)
-  end
-
-  def echo2(channel_id, received_text)
-    # 如果在 channel_id 最近沒人講過 received_text，卡米狗就不回應
-    recent_received_texts = Received.where(channel_id: channel_id).last(5)&.pluck(:text)
-    return nil unless received_text.in? recent_received_texts
-    
-    # 如果在 channel_id 卡米狗上一句回應是 received_text，卡米狗就不回應
-    last_reply_text = Reply.where(channel_id: channel_id).last&.text
-    return nil if last_reply_text == received_text
-
-    received_text
   end
 
   # 關鍵字回覆
